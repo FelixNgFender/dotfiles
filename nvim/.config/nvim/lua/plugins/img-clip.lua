e7da1217bc578dc5636be191b2eba0a10343e207
felixngfender <ngthinh302@gmail.com>
2025-05-13 21:23:18 -0700

    add img-clip

---
+++ nvim/.config/nvim/lua/plugins/img-clip.lua
@@ -0,0 +1,16 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "VeryLazy",
+  opts = {
+    -- add options here
+    -- or leave it empty to use the default settings
+    default = {
+      relative_to_current_file = true,
+      show_dir_path_in_prompt = true,
+    },
+  },
+  keys = {
+    -- suggested keymap
+    { "<leader>p", "<cmd>PasteImage<cr>", desc = "Paste image from system clipboard" },
+  },
+}
