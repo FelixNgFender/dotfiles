ad83afc5b1c0724d7412283abd4cbfa78b4ba85a
felixngfender <ngthinh302@gmail.com>
2025-08-21 19:55:20 -0400

    typst >>> latex

--- nvim/.config/nvim/lua/plugins/typst-preview.lua
+++ nvim/.config/nvim/lua/plugins/typst-preview.lua
@@ -1,15 +1,56 @@
 return {
-	"chomosuke/typst-preview.nvim",
-	ft = "typst",
-	version = "1.*",
-	opts = {
-		dependencies_bin = {
-			["tinymist"] = "tinymist",
-			["websocat"] = "websocat",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = {
+			ensure_installed = { "typst" },
 		},
-		open_cmd = "firefox %s",
-	}, -- lazy.nvim will implicitly calls `setup {}`
-	keys = {
-		{ "<leader>cp", "<cmd>TypstPreview<CR>", desc = "TypstPreviewToggle" },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				tinymist = {
+					single_file_support = true, -- Fixes LSP attachment in non-Git directories
+					settings = {
+						formatterMode = "typstyle",
+						exportPdf = "onSave",
+					},
+					lint = {
+						enabled = true,
+					},
+				},
+			},
+		},
+	},
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters_by_ft = {
+				typst = { "typstyle", lsp_format = "prefer" },
+			},
+		},
+	},
+	{
+		"chomosuke/typst-preview.nvim",
+		ft = "typst",
+		version = "1.*",
+		opts = {
+			dependencies_bin = {
+				["tinymist"] = "tinymist",
+				["websocat"] = "websocat",
+			},
+			open_cmd = "firefox %s",
+		}, -- lazy.nvim will implicitly calls `setup {}`
+		keys = {
+			{ "<leader>cp", ft = "typst", "<cmd>TypstPreviewToggle<CR>", desc = "Toggle Typst Preview" },
+		},
+	},
+	{
+		"folke/ts-comments.nvim",
+		opts = {
+			lang = {
+				typst = { "// %s", "/* %s */" },
+			},
+		},
 	},
 }
