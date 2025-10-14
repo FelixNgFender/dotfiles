2f4e73d155ee8ec8c3a9f04c910cf06b1239bf42
felixngfender <ngthinh302@gmail.com>
2025-10-13 23:08:06 -0400

    avoid copilot toggle collision with sidekick.nvim

--- nvim/.config/nvim/lua/plugins/copilot.lua
+++ nvim/.config/nvim/lua/plugins/copilot.lua
@@ -1,16 +1,16 @@
 return {
-  "zbirenbaum/copilot.lua",
-  keys = {
-    {
-      "<leader>at",
-      function()
-        if require("copilot.client").is_disabled() then
-          require("copilot.command").enable()
-        else
-          require("copilot.command").disable()
-        end
-      end,
-      desc = "Toggle (Copilot)",
-    },
-  },
+	"zbirenbaum/copilot.lua",
+	keys = {
+		{
+			"<leader>aT",
+			function()
+				if require("copilot.client").is_disabled() then
+					require("copilot.command").enable()
+				else
+					require("copilot.command").disable()
+				end
+			end,
+			desc = "Toggle (Copilot)",
+		},
+	},
 }
