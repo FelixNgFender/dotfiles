968efddb3044a3975ab857b716a7bc8d48b0f312
felixngfender <ngthinh302@gmail.com>
2026-06-27 23:33:41 -0700

    format

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,11 +1,11 @@
 -- lua/plugins/which-key.lua
 return {
-  "folke/which-key.nvim",
-  opts = {
-    icons = {
-      rules = {
-        { plugin = "copilot.lua", icon = " ", color = "orange" },
-      },
-    },
-  },
+	"folke/which-key.nvim",
+	opts = {
+		icons = {
+			rules = {
+				{ plugin = "copilot.lua", icon = " ", color = "orange" },
+			},
+		},
+	},
 }
